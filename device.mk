f92769e9e3042c445da69289df1ffea9cbc26997
Marco Zanin <mrczn.bb@gmail.com>
2023-02-27 17:33:05 +0100

    grus: clean overlays
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 # QDCM
 PRODUCT_COPY_FILES += \
